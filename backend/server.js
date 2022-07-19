7937ff26e4474951f7f06ebbfc65bbb442b4f354
Henrique Moreira <riqimoreiraa@gmail.com>
2022-07-18 22:21:32 -0300

    dep

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./database/connect');
+const path = require('path');
 
 app.use(cors())
 connectDB();
@@ -16,6 +17,18 @@ app.use(express.urlencoded( { extended: false } ));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
