9798dc1faa4876b5ab06980327a8f5b4baa88a8f
Henrique Moreira <riqimoreiraa@gmail.com>
2022-07-19 12:48:43 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = `${process.env.REACT_APP_API_URL}/api` || 'http://localhost:5000/api'
+const url = `${process.env.REACT_APP_API_URL}api` || 'http://localhost:5000/api'
 
 export default axios.create({
     baseURL: `${url}`,
